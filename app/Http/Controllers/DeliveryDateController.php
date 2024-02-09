f15ff6ff49ed2f3c3057f495118a7845cb984e2a
pmhr18 <tmh6.pm18@gmail.com>
2024-02-10 08:33:58 +0900

    add/fix (3)発送締め切り時間設定を実装、タイムゾーン修正、現在時間取得コードを簡略化

--- app/Http/Controllers/DeliveryDateController.php
+++ app/Http/Controllers/DeliveryDateController.php
@@ -13,12 +13,29 @@ class DeliveryDateController extends Controller
         // 設定値を取得
         $fastestDeliveryDate = Config::get('myconfig.fastest_delivery_date');
         $outputDays = Config::get('myconfig.output_days');
+        $shippingDeadline = Config::get('myconfig.shipping_deadline');
 
         // 配送候補日を生成
         $fetchDeliverySlecetDays = [];
+        
+        // 設定(1)
         // 最短発送日の値により開始日を設定
         $startDate = Carbon::now()->addDays($fastestDeliveryDate)->startOfDay();
 
+        // 現在時刻を取得
+        $currentTime = Carbon::now()->hour;
+
+        // 設定(3)
+        // 発送締切設定が true の場合のみ、締切設定時間と現在時間を比較する
+        if ($shippingDeadline['is_effective'] === true) {
+            if ($currentTime >= $shippingDeadline['deadline_hour']) {
+                // 締切設定時間を過ぎている場合のみ、最短発送日を設定日分後ろ倒す
+                $delayDays = $shippingDeadline['delay_date'];
+                $startDate->addDays($delayDays);
+            }
+        }
+
+        // 設定(2)
         // 出力日数の値分配送候補日を取得し、追加する
         for ($i = 0; $i < $outputDays; $i++) {
             $fetchDeliverySlecetDays[] = $startDate->format('Y-m-d');
--- config/myconfig.php
+++ config/myconfig.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
-    'fastest_delivery_date' => 1,
-    'output_days' => 5
+    'fastest_delivery_date' => 1,       // 設定値(1)
+    'output_days' => 5,                 // 設定値(2)
+    'shipping_deadline' => [            // 設定値(3)
+        'deadline_hour' => 15,
+        'delay_date' => 1,
+        'is_effective' => true
+    ]
 ];
